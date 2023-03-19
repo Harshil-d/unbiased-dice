d466299f40e227066ac310f379c561658dbe2704
Harshil-d <zadafiyaharshil@gmail.com>
2023-03-19 13:57:54 +0530

    first draft.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,29 @@
-import { getRandomArbitrary } from "./utils";
+import { getRandomArbitrary, readData, sleep, writeData } from './utils';
 
-// console.log(generateRandom(1, 6));
-console.log(getRandomArbitrary(1, 6));
+const testNumber = 10;
+
+function main() {
+  const diceOneId = '7db0823c';
+  const diceTwoId = '1e6eef2d';
+  const data = readData();
+  let count = 0;
+
+  if (data[diceOneId] == undefined) {
+    data[diceOneId] = [];
+  }
+  if (data[diceTwoId] == undefined) {
+    data[diceTwoId] = [];
+  }
+
+  while (count < testNumber) {
+    const onTop = getRandomArbitrary(1, 6);
+    data[diceOneId].push(onTop);
+    console.log('On Top: ', onTop);
+    count++;
+    sleep(2000);
+  }
+
+  writeData(data);
+}
+
+main();
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,25 @@
+import * as fs from 'fs';
+import path from 'path';
+
+const filePath = 'predictionData.json';
+const absolutePath = path.join(__dirname, '..', 'data', filePath);
+
 export function getRandomArbitrary(min, max) {
   return Math.round(Math.random() * (max - min) + min);
 }
 
+export function readData() {
+  try {
+    return JSON.parse(fs.readFileSync(absolutePath).toString());
+  } catch (error) {
+    return {};
+  }
+}
+
+export function writeData(data: object) {
+  return fs.writeFileSync(absolutePath, JSON.stringify(data), { flag: 'a+' });
+}
+
+export const sleep = (ms) => {
+  return new Promise((resolve, reject) => setTimeout(resolve, ms));
+};
